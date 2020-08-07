55554b903f1b0ba4112217e9085d877401b55524
Alex Skreen <skreenalex@gmail.com>
2020-08-07 16:19:35 -0700

    add images

--- src/components/contact.js
+++ src/components/contact.js
@@ -15,11 +15,7 @@ class Contact extends Component {
               style={{ height: "250px" }}
             />
             <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries
+              I am currently seeking a position as a full-stack developer. Please feel free to contact me if after reviewing my portfolio you think I would be a good fit at your company. I look forward to speaking with you.
             </p>
           </Cell>
           <Cell col={6}>
